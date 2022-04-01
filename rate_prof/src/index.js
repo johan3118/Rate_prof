63da87cc3acc31f6b3f788afcc5862a5d2002055
Johan Contreras <cepedaabiel@gmail.com>
2022-04-01 10:26:44 -0400

    perfil de profesores

--- rate_prof/src/index.js
+++ rate_prof/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Perfil_profesor from './Perfil_profesor';
 
 ReactDOM.render(
   <React.StrictMode>
