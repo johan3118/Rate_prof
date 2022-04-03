6b3e08593bf7de83c434d1304a2111ddf3b5e535
Gleidy Contreras <gleidy0105@gmail.com>
2022-04-03 02:40:33 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import { app } from './Firebase.js';
 import AddTeacher from "./AddTeacher.js";
-
+import PerfilProfesor from "./Perfil_profesor";
 function App() {
   return (
-      <h1>
-        Hola Johan
-      </h1>
+    <PerfilProfesor/>
   );
 }
 
